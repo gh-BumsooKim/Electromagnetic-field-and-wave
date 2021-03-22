ecf30d2b912ad5ce0713ff7616dfaa28ddabfb7d
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-03-22 10:07:40 +0900

    Update Practicum02-Calculation_of_E-field_for_uniform_line_charge_on_finite_length.m

--- Practicum02-Calculation_of_E-field_for_uniform_line_charge_on_finite_length.m
+++ Practicum02-Calculation_of_E-field_for_uniform_line_charge_on_finite_length.m
@@ -1,13 +1,65 @@
-clear all
-close all
-clc
+clear all, close all, clc
 
-x = 0.1:0.1:2;
-z = -2.5:0.1:2.5;
+%% Constant
+eps0 = 8.854e-12;
 
-[X, Z] = meshgrid(x,z);
+%% Inputs
+rhoL = 10e-9;
+L    = 3;
+dz   = 0.1;
+drho = 0.02;
+zmin = -2.5;
+zmax = 2.5;
+rhomin = 0.1;
+rhomax = 0.5;
 
-rho_L = 10;
-E = (rho_L/30)/(4*pi*eps)*(X./X.^2);
+%% Calculation
+Ca = rhoL/(4*pi*eps0);
 
-mesh(X,E,Z);
+%% Analytic Solution
+%z = zmin:dz:zmax;
+%rho = rhomin:drho:rhomax;
+Nz = (zmax-zmin)/dz+1;
+Nrho = (rhoamx-rhomin)/drho+1;
+
+for ii=1:Nz
+    zii = (ii-1)*dz;
+    z(ii) = zii;
+    for jj=1:Nrho
+        rhojj = jj*drho;
+        rho(jj) = rhojj;
+        
+        R1 = sqrt((L-zii)^2+rhojj^2);
+        R2 = sqrt(zii^2+rhojj^2);
+        sinbeta1 = (L-zii)/R1;
+        sinbeta2 = zii/R2;
+        cosbeta1 = sqrt(1-sinbeta1^2);
+        cosbeta2 = sqrt(1-sinbeta2^2);
+        Erho1 = Ca*sinbeta1/rhojj;
+        Ez1 = Ca*(cosbeta1-1)/rhojj;
+        Erho2 = Ca*sinbeta2/rhojj;
+        Ez2 = Ca*(cosbeta2-1)/rhojj;
+        
+        Erho(ii,jj) = Erho1+Erho2;
+        Ez(ii,jj) = Ez1-Ez2;
+    end
+end
+
+figure(1)
+Nzm = 8;
+Erhom1 = Erho(Nzm,:);
+Ezm1 = Ez(Nzm,:);
+subplot(2,1,1), plot(rho,Erhom1,rho,Ezm1)
+
+Nrhom = 10;
+Erhom2 = Erho(:,Nrhom);
+Ezm2 = Ez(:,Nrhom);
+subplot(2,1,2), plot(z,Erhom2,rho,Ezm2)
+
+figure(2)
+subplot(2,1,1), pcolor(rho,z,Erho), shading interp, colorbar
+subplot(2,1,2), pcolor(rho,z,Ez), shading interp, colorbar
+%subplot(2,1,1), pcolor(rho,z,Erho), axis equal, shading interp, colorbar
+%subplot(2,1,2), pcolor(rho,z,Ez), axis equal, shading interp, colorbar
+
+%% Numerical Solution
